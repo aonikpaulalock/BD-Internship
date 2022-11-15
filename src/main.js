6ddeb1e6b54c961e900033e3a14708786b4b4998
aonikpaulalock <alockpaul9845@gmail.com>
2022-11-15 22:48:35 +0600

    deploying vue projects

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
-import "bootstrap/dist/css/bootstrap.css"
+import "bootstrap/dist/css/bootstrap.css" // bootstrap css file
 import { createApp } from 'vue'
 import './style.scss'
 import App from './App.vue'
 
 createApp(App).mount('#app')
 
+// bootstrap js file
 import "bootstrap/dist/js/bootstrap.js"
