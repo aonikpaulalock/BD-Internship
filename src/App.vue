091ca726c8abaa01680a167dd24fc93c3794715a
aonikpaulalock <alockpaul9845@gmail.com>
2022-11-15 13:13:50 +0600

    compleated slidebar

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,88 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-   <div>
-    <h1 class="text-success">gggggggggggggggg</h1>
-   </div>
+  <section class="dashboard-container">
+   <header class="header-container">
+   <nav class="navbar navbar-expand-lg bg-transparent navber-custom p-0">
+  <div class="container-fluid p-0">
+   <form class="">
+        <img src="./assets/Svg/Vector.svg" alt="" class="position-absolute ps-4 svg-vactor">
+        <input class="form-control input-field" type="search" placeholder="Search Here ....">
+      </form>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-item" aria-current="page" href="#">
+            <img src="./assets/Svg/45-chat.svg" alt="">
+          </a>
+        </li>
+        <li class="nav-item mx-5">
+          <img src="./assets/Svg/announcement.svg" alt="">
+        </li>
+        <li class="nav-item">
+          <img src="./assets/Svg/Setting.svg" alt="">
+        </li>
+        <div class="nav-item ms-5">
+         <div class="d-flex">
+          <div class="image-admin me-4">
+          </div>
+          <div>
+            <h5>Madison Eve</h5>
+            <h6>Admin</h6>
+          </div> 
+         </div>
+        </div>
+      </ul>
+    </div>
+  </div>
+</nav>
+<main class="content text-center">
+   <h1>main content</h1>
+</main>
+   </header>
+   <aside class="slidebar-container">
+    <ul class="logo-container">
+      <li class="dasboard-main-icon">
+       <img src="./assets/Svg/logo.svg" alt="">
+      </li>
+      <li class="dasboard-icon">
+       <img src="./assets/Svg/Home 1.svg" alt="">
+      </li>
+      <li class="dasboard-icon">
+       <img src="./assets/Svg/1-mail.svg" alt="">
+      </li>
+      <li class="dasboard-icon">
+       <img src="./assets/Svg/47-team.svg" alt="">
+      </li>
+      <li class="dasboard-icon">
+       <img src="./assets/Svg/20-dollar simbol.svg" alt="">
+      </li>
+      <li class="dasboard-icon">
+       <img src="./assets/Svg/46-task.svg" alt="">
+      </li>
+      <li class="dasboard-icon">
+       <img src="./assets/Svg/4-list.svg" alt="">
+      </li>
+      <li class="dasboard-icon">
+       <img src="./assets/Svg/Wallet.svg" alt="">
+      </li>
+      <li class="dasboard-icon">
+       <img src="./assets/Svg/25-credit card.svg" alt="">
+      </li>
+      <li class="dasboard-icon">
+       <img src="./assets/Svg/34-folder.svg" alt="">
+      </li>
+      <li class="dasboard-icon">
+       <img src="./assets/Svg/2-user.svg" alt="">
+      </li>
+      <li class="dasboard-icon">
+       <img src="./assets/Svg/Calendar.svg" alt="">
+      </li>
+      <li class="dasboard-icon">
+       <img src="./assets/Svg/10-todo.svg" alt="">
+      </li>
+    </ul>
+   </aside>
+  </section>
 </template>
-
-<style scoped>
-
-</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
+import "bootstrap/dist/css/bootstrap.css"
 import { createApp } from 'vue'
 import './style.scss'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+import "bootstrap/dist/js/bootstrap.js"
